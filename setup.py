e8a4c2d8b4e8711896d074a9f1862a2d303a072d
perrydc <perrydc@gmail.com>
2017-03-28 08:29:43 -0400

    futurize

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from future import standard_library
+standard_library.install_aliases()
 
 def readme():
     with open('README.rst') as f:
@@ -6,13 +8,19 @@ def readme():
 
 setup(name='npr',
       version='0.1',
-      description='NPR API services framework',
+      description='NPR cloud framework',
       long_description='Self-authenticating module for accessing NPR APIs in Python.',
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Internet :: WWW/HTTP',
       ],
       keywords='public radio stream metadata api service',
       url='http://github.com/perrydc/npr',
