3b72bd2334dcd72b71bb7bb92e1ab62205a30e1c
perrydc <perrydc@gmail.com>
2017-04-29 20:27:24 -0400

    removed package files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='npr',
-      version='1.0.0',
+      version='1.0.7',
       description='NPR cloud framework',
       long_description='Self-authenticating module for accessing NPR APIs in Python.',
       classifiers=[
