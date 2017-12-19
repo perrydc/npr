ba5ff76033da9a1e458965b29a7462463f3ced91
Demian Perry <perrydc@gmail.com>
2017-12-19 17:17:04 -0500

    2.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='npr',
-      version='2.0.0',
+      version='2.0.1',
       description='NPR cloud framework',
       long_description=read('README.rst'),
       classifiers=[
